b6b7b82bc1b053adc2fee62b90162aab21e55064
Nathan Ricci <naricc@microsoft.com>
2021-05-24 16:15:08 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace out_of_tree_wasm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
